68768d8eba4524f856dc1f1dafe741f2a794bd23
reetah589 <reetah589@gmail.com>
2021-10-26 17:48:06 -0500

    initial game setup

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -43,15 +44,28 @@ var fight = function() {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
         // if player choses to skip 
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert(playerName + " has chosen to skip the fight!");
-    } 
-    else {
+        } else if (promptFight === "skip" || promptFight === "SKIP") {
+
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit");
+
+        // if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skipt this fight. Goodbye!");
+            // subtract money from playerMoney for skipping
+            playerMoney = playerMoney - 2;
+        }
+
+        // if no (false), ask question again by running fight() again 
+        else {
+            fight();
+        }
+        } else {
         window.alert("You need to choose a valid option. Try again!");
     }
 
 
- //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+ // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
     enemyHealth = enemyHealth - playerAttack;
 
  // Log a resulting message to the console so we know that it worked.
